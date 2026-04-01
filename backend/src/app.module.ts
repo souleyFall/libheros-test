db5708f32d2237914166869d979b9e58fd8fa0e5
Souleymane FALL <12113387@edu.sorbonne-paris-nord.fr>
2026-04-01 15:01:19 +0200

    feat : cleaning code & fix delete tasks bug

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -17,11 +17,11 @@ import { Task } from './task/task.model';
 
     SequelizeModule.forRoot({
       dialect: 'mysql',
-      host: process.env.DB_HOST || 'localhost',
-      port: Number(process.env.DB_PORT) || 3306,
-      username: process.env.DB_USER || 'root',
-      password: process.env.DB_PASSWORD || '',
-      database: process.env.DB_NAME || 'libheros',
+      host: process.env.DB_HOST,
+      port: Number(process.env.DB_PORT),
+      username: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
       models: [User, List, Task], 
       autoLoadModels: true, 
       synchronize: true,    
